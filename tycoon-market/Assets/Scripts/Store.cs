7c7170406bde7a4c1644d1530e8922467e1f6093
Maniok19 <mano19029@hotmail.fr>
2020-12-20 02:41:59 +0100

    joyeunoel
    mouais bon debut

---
+++ tycoon-market/Assets/Scripts/Store.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Store : MonoBehaviour
+{
+   
+    float BaseStoreCost;
+    float BaseStoreProfit = 2f;
+    public Slider ProgressSlider;
+    public gameManager GameManager;
+    int StoreCount;
+    public Text StoreCountText;
+   
+
+    float StoreTimer = 2f;
+    float CurrentTimer = 0;
+    bool StartTimer=false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StoreCount = 1;
+
+        BaseStoreCost = 1.5f;
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (StartTimer)
+        {
+            CurrentTimer += Time.deltaTime;
+            if (CurrentTimer > StoreTimer)
+            {
+                StartTimer = false;
+                CurrentTimer = 0f;
+                
+                    GameManager.AddToBalance(BaseStoreProfit * StoreCount);
+            }
+            ProgressSlider.value = CurrentTimer / StoreTimer;
+        }
+    }
+    public void BuyOnClick ()
+    {
+        if (!GameManager.CanBuy(BaseStoreCost))
+            return;
+        StoreCount++;
+        Debug.Log(StoreCount);
+        StoreCountText.text = StoreCount.ToString();
+            GameManager.AddToBalance(- BaseStoreCost);
+       
+    }
+    public void StoreOnClick()
+    {
+        if (!StartTimer)
+        { StartTimer = true; }
+    }
+}
---
+++ tycoon-market/Assets/Scripts/gameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class gameManager : MonoBehaviour
+{
+    float CurrentBalance;
+    public Text CurrentBalanceText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CurrentBalance = 5;
+        CurrentBalanceText.text = CurrentBalance.ToString("C2");
+    }
+
+    public void AddToBalance(float amt)
+    {
+        CurrentBalance += amt;
+        CurrentBalanceText.text = CurrentBalance.ToString("C2");
+    }
+    public bool CanBuy(float AmtToSpend)
+    {
+        if (AmtToSpend > CurrentBalance)
+            return false;
+        else
+            return true;
+    }
+
+}
+
+
+
